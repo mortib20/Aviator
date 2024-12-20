212589ebd9886e256237ea020977bc7f117c4a6e
Martin Paul <martin.paul@vogler-engineering.de>
2024-12-20 10:22:19 +0100

    added missing metrics file

---
+++ Aviator.Acars/Metrics/InfluxDBMetrics.cs
@@ -0,0 +1,22 @@
+﻿using Aviator.Acars.Config;
+using Aviator.Acars.Entities;
+using InfluxDB3.Client;
+using InfluxDB3.Client.Write;
+
+namespace Aviator.Acars.Metrics;
+
+public class InfluxDbMetrics(InfluxDbConfig influxConfig) : IAcarsMetrics
+{
+    private readonly InfluxDBClient _client = new(host: influxConfig.Url, token: influxConfig.Token,
+        organization: influxConfig.Organization, influxConfig.Bucket);
+
+    public async Task Increase(AcarsType type, BasicAcars acars, CancellationToken cancellationToken = default)
+    {
+        var point = PointData.Measurement("messages")
+            .SetTag("type", type.ToString())
+            // TODO CHANNEL TIMESTAMP
+            .SetField("value", 1);
+
+        await _client.WritePointAsync(point, cancellationToken: cancellationToken);
+    }
+}
