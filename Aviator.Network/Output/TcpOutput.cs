a94e0b2f83a2586802a39700c512748fcea6707a
Martin Paul <martin.paul-3277@web.de>
2024-12-22 20:41:39 +0100

    fix?

--- Aviator.Network/Output/TcpOutput.cs
+++ Aviator.Network/Output/TcpOutput.cs
@@ -1,64 +1,74 @@
 ﻿using System.Net.Sockets;
 using Microsoft.Extensions.Logging;
+using Timer = System.Timers.Timer;
 
 namespace Aviator.Network.Output;
 
-public sealed class TcpOutput(string host, int port, ILogger<TcpOutput> logger) : IOutput, IDisposable
+public sealed class TcpOutput : IOutput, IDisposable
 {
     private static readonly TimeSpan ErrorTimeout = TimeSpan.FromSeconds(4);
+    private static Timer _connectionTimer = new(ErrorTimeout);
 
-    private TcpClient _client = new(host, port);
+    private TcpClient? _client;
     private bool _connected;
     private readonly SemaphoreSlim _connectionLock = new(1, 1);
-    
+    private readonly string _host;
+    private readonly int _port;
+    private readonly ILogger<TcpOutput> _logger;
+
+    public TcpOutput(string host, int port, ILogger<TcpOutput> logger)
+    {
+        _host = host;
+        _port = port;
+        _logger = logger;
+        EndPoint = $"{host}:{port}";
+
+        _connectionTimer.Elapsed += async (sender, args) =>
+        {
+            if (_connected)
+            {
+                return;
+            }
+            
+            try
+            {
+                _client = new TcpClient();
+                await _client.ConnectAsync(host, port);
+                _connected = _client.Connected;
+            }
+            catch (Exception ex)
+            {
+                _connected = false;
+                _logger.LogWarning(ex, "Client failed to connect or got disconnected from {Host}:{Port}, waiting for {ErrorTimeout} seconds!", _host, _port, ErrorTimeout.TotalSeconds);
+            }
+            
+        };
+    }
+
     public void Dispose()
     {
         Dispose(true);
         _connectionLock.Dispose();
     }
 
-    public string EndPoint { get; init; } = $"{host}:{port}";
+    public string EndPoint { get; init; }
 
     public async ValueTask WriteAsync(byte[] buffer, CancellationToken cancellationToken = default)
     {
-        if (_connectionLock.CurrentCount == 0)
+        if (_client is null || !_connected)
         {
             return;
         }
         
         try
         {
-            // Ensure only one reconnect attempt occurs
-            if (!_connected)
-            {
-                await _connectionLock.WaitAsync(cancellationToken).ConfigureAwait(false);
-                try
-                {
-                    if (!_connected) // Check again after acquiring the lock
-                    {
-                        logger.LogInformation("Connecting to {Hostname}:{Port}", host, port);
-                        _client = new TcpClient();
-                        await _client.Client.ConnectAsync(host, port, cancellationToken).ConfigureAwait(false);
-                        _connected = true;
-                    }
-                    else
-                    {
-                        return;
-                    }
-                }
-                finally
-                {
-                    _connectionLock.Release();
-                }
-            }
-
             await _client.GetStream().WriteAsync(buffer, cancellationToken).ConfigureAwait(false);
             await Task.Delay(TimeSpan.FromMilliseconds(100), cancellationToken).ConfigureAwait(false);
         }
         catch (Exception ex) when (ex is IOException or SocketException)
         {
             _connected = false;
-            logger.LogWarning(ex, "Client failed to connect or got disconnected from {Host}:{Port}, waiting for {ErrorTimeout} seconds!", host, port, ErrorTimeout.TotalSeconds);
+            _logger.LogWarning(ex, "Client failed to connect or got disconnected from {Host}:{Port}, waiting for {ErrorTimeout} seconds!", _host, _port, ErrorTimeout.TotalSeconds);
         }
     }
 
